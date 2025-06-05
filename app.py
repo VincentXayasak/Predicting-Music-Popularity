e864e653e824451168718e143bd7e0b2241e2264
Vincent Xayasak <xayasakvincent@gmail.com>
2025-06-04 23:50:31 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,23 +1,22 @@
 import gradio as gr
 import pandas as pd
 import joblib
+import os
 
-# Load model
-pipeline = joblib.load("lgbm_pipeline.pkl")
+# Path on Hugging Face will be in root dir
+model_path = os.path.join(os.path.dirname(__file__), "lgbm_pipeline.pkl")
+pipeline = joblib.load(model_path)
 
-# Predict function
 def predict_popularity(file):
     df = pd.read_csv(file.name)
     predictions = pipeline.predict(df).tolist()
     return {"predictions": predictions}
 
-# Gradio interface
 app = gr.Interface(
     fn=predict_popularity,
-    inputs=gr.File(label="Upload CSV with Song Features"),
+    inputs=gr.File(label="Upload CSV"),
     outputs=gr.JSON(label="Predicted Popularity"),
-    title="Spotify Song Popularity Predictor",
-    description="Upload a CSV with the appropriate features to predict song popularity using a LightGBM model."
+    title="Spotify Song Popularity Predictor"
 )
 
 app.launch()
